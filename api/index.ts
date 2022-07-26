965364530cd0dd33f59c1af63388291d4a2daa48
Compdog Inc <compdog.inc@gmail.com>
2022-07-26 00:39:31 -0400

    Update action version

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import type { NowRequest, NowResponse } from '@vercel/node';
 export default function (_: NowRequest, res: NowResponse) {
     res.json({
         result: "ok"
-    })
+    });
 }
 
 //git comment
